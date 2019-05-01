35d6a938d0ab4a4d0b289546d74b389cdfab486f
Sufian Javed <sufian.javed94@gmail.com>
2019-05-01 13:11:22 -0400

    Hooking up app to redux

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,5 +1,11 @@
-const initState = {}
-
+const initState = {
+    projects: [
+        {id: '1', title: 'Step 1', content: 'Complete signup pages'},
+        {id: '2', title: 'Step 2', content: 'Complete signin page'},
+        {id: '3', title: 'Step 3', content: 'Hook up app with Firebase'}
+    ]
+}
+    
 const projectReducer = (state = initState, action) => {
     return state
 }
